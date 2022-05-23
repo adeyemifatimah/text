5ff69677942ab2bfa20d6e70d1da7cad28345dd1
abisoye <beesoye111@gmail.com>
2022-05-23 11:47:36 +0100

    added image to folder

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,26 @@
 $(document).ready(function () {
     $("#text").submit(function (event) {
         event.preventDefault();
-       
+        let sent = $("input#sent").val()
+        let arr = sent.split(' ');
+
+        let arr1 = 3;
 
 
+        let result = [];
 
+        arr.forEach(function (string) {
+            if (string.length > arr1) {
+                result.push(string);
+            }
 
+        });
 
-
-
-
-
+        let newArr = result.reverse()
+        let newArr2 = newArr.join(",");
+        $("#put").append(newArr2);
 
 
 
     });
-
-
 });
